624b97071f5ad03835cce64d9423a79f0da4ecee
eMerzh <brice@bmaron.net>
2024-04-15 23:32:07 +0200

    feat: use vite instead of esbuild

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig, loadEnv } from 'vite'
+import preact from "@preact/preset-vite";
+
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '')
+
+	return {
+		// vite config
+		define: {
+			__MAPBOX_TOKEN__: JSON.stringify(env.MAPBOX_TOKEN),
+		},
+		plugins: [preact()],
+		base: 'gtfs-rt-inspector',
+		optimizeDeps: {
+			esbuildOptions: {
+				loader: {
+					'.js': 'jsx',
+				},
+			},
+		},
+	}
+})
